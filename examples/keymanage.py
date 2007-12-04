d4a3244e7ba281d6dd1ca4960b3e3e10ef2df000
sophacles <sophacles@9b23d847-d119-0410-8f47-b34f0ca21d1e>
2007-12-04 15:42:07 +0000

    debugging of key mangement example util

--- examples/keymanage.py
+++ examples/keymanage.py
@@ -1,13 +1,16 @@
 import sys, optparse
+# XXX
+# For debug purposes only, remove when setup.py is incorporated
+sys.path.insert(0,'..')
 from isbndb import keys
 
 def handleopts():
     usage = '''keytool [option] [keyfile]
 No options defaults to list keys'''
     parser = optparse.OptionParser(usage)
-    parser.add_option('-a', '--add', dest='add', default = ''
+    parser.add_option('-a', '--add', dest='add', default = '',
         help="Add a key to the keyfile")
-    parser.add_option('-r', '--remove', dest='remove', default = ''
+    parser.add_option('-r', '--remove', dest='remove', default = '',
         help="Remove specified key from the keyfile")
     parser.add_option('-l', '--list', dest='list', action='store_true',
         default=True, help='List keys in the file')
@@ -21,17 +24,20 @@ def main():
     keys.loadKeys(kfname)
 
     if options.add:
-        try:
-            keys.addKey(options.add)
-        except ValueError:
+        if keys.addKey(options.add):
+            print "Successfully added key: %s" % (options.add)
+            keys.saveKeys()
+            sys.exit(0)
+        else:
             print "Invalid key: %s, exiting" % (options.add)
             sys.exit(1)
-        print "Successfully added key: %s" % (options.add)
+
     elif options.remove:
         # Note on implementation this will call keys.remove(key) in a try
         # block like above
         print "Option currently disabled, key removal is not yet implemented"
         sys.exit(1)
+
     else:
         i = 0
         for k in keys._KEYS:
